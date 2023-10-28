5f1d5a547f1678e7566a262818c885f9a61db1c8
Luis Henrique Paoletti <e11929823@student.tuwien.ac.at>
2023-10-29 01:57:32 +0200

    added helper functions: `trace`, `usage`, and `abort_mydiff`

--- mydiff.c
+++ mydiff.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
 
+void trace(char *str) {
+    fprintf(stderr, "%s\n", str);
+    fflush(stderr);
+}
+
+
+void usage(void) {
+    fprintf(stderr, "mydiff [-i] [-o outfile] file1 file2\n");
+}
+
+
+void abort_mydiff(char *str) {
+    fprintf(stderr, "%s.\n", str);
+    usage();
+}
+
+
 int main(int argc, char** argv) {
     printf("Hello world\n");
     return 0;
